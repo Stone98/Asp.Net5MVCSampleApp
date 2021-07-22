be26e3fa04480cd0480c97e047fe34b269113a4a
Stone Cogswell <70171431+Stone98@users.noreply.github.com>
2021-07-22 16:07:13 -0700

    Add project files.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            this.ViewData["Title"] = "Borrowed Items";
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+        // Get-Create
+        public IActionResult Create()
+        {
+            this.ViewData["Title"] = "Create Borrowed Item";
+            return View();
+        }
+
+        // Post-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            this.ViewData["Title"] = "Create Borrowed Item";
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
