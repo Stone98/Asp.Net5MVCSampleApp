49809ac172b6bd66f587ab9c036a582cff1bfde6
Stone Cogswell <70171431+Stone98@users.noreply.github.com>
2021-08-09 16:42:23 -0700

    added expense types page

---
+++ Controllers/ExpenseTypeController.cs
@@ -0,0 +1,111 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            this.ViewData["Title"] = "Expense Types";
+            IEnumerable<ExpenseType> objList = _db.ExpenseTypes;
+            return View(objList);
+        }
+
+        // Get-Create
+        public IActionResult Create()
+        {
+            this.ViewData["Title"] = "Create Expense Type";
+            return View();
+        }
+
+        // Post-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ExpenseType obj)
+        {
+            this.ViewData["Title"] = "Create Expense Type";
+            if (ModelState.IsValid)
+            {
+                _db.ExpenseTypes.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Get-Delete
+        public IActionResult Delete(int? id)
+        {
+            this.ViewData["Title"] = "Delete Expense Type";
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ExpenseTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // Post-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ExpenseTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ExpenseTypes.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Get-Update
+        public IActionResult Update(int? id)
+        {
+            this.ViewData["Title"] = "Update Expense Type";
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ExpenseTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // Post-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(ExpenseType obj)
+        {
+            this.ViewData["Title"] = "Update Expense Type";
+            if (ModelState.IsValid)
+            {
+                _db.ExpenseTypes.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
